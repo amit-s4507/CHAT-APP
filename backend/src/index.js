b63dcab05e292451611a3985aa147faae8e10cfc
Amit Sahu <amitsahu174507@gmail.com>
2025-03-12 02:49:59 +0530

    changed

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv"
 
+import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 const app = express();
@@ -12,4 +14,6 @@ app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
     console.log("Server is running on PORT :" + PORT);
+    connectDB()
+
 });
